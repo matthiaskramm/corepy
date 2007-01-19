fa3fe51724ab99451e895eb8c64c6e34c98b5940
benjmart <benjmart@indiana.edu>
2007-01-19 14:22:19 +0000

    Moved new version of spu stuff to arch/spu/platform/linux_spufs/

---
+++ corepy/arch/spu/platform/linux_spufs/synbuffer.c
@@ -0,0 +1,34 @@
+// Copyright 2006 The Trustees of Indiana University.
+
+// This software is available for evaluation purposes only.  It may not be
+// redistirubted or used for any other purposes without express written
+// permission from the authors.
+
+// Author:
+//   Christopher Mueller
+
+// Helpers for working with Buffers
+
+#include <Python.h>
+
+static PyObject *
+synbuffer_buffer_info(PyObject *self, PyObject *args) {
+  char *addr;
+  int size;
+  if (!PyArg_ParseTuple(args, "w#", &addr, &size))
+    return NULL;
+
+  return Py_BuildValue("(ii)", (unsigned int)addr, size);
+}
+
+static PyMethodDef synbufferMethods[] = {
+    {"buffer_info",  synbuffer_buffer_info, METH_VARARGS, "Return the addres and size of a buffer."},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+PyMODINIT_FUNC
+initsynbuffer(void)
+{
+    (void) Py_InitModule("synbuffer", synbufferMethods);
+}
+
